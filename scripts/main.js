eb3ee778e86be27e2d5d5d4a6f7eaaa172fa2199
Vitaly-Future-Developer <zarvoron@yandex.ru>
2020-02-12 19:21:34 +0500

    События на скролл при включённом glass-block

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ $('#about_us').on('click', function(evt){
 	try{
 		$('.page-header').toggleClass('page-header--glass');
 		$('.glass-block').toggleClass('glass-block--closed glass-block--opened');
-		//$('body').removeClass('background--gradient');
+
 		$('.page-footer').toggleClass('page-footer--opened');
 		$('.page-main__header-title').addClass('page-main__header-title--glass');
 	} catch {
@@ -14,22 +14,31 @@ $('#about_us').on('click', function(evt){
 });
 
 //Клик на scroll
-$('.page-footer__scroll-btn').on('click', function(evt){
+function scroll(evt){
 	evt.preventDefault();
 	try{
-		$('.page-header').removeClass('page-header--glass');
-		$('.page-header').addClass('page-header--transparent');
 
-		$('.glass-block').removeClass('glass-block--closed glass-block--opened');
-		$('.glass-block').addClass('glass-block--closed');
-
-		$('body').addClass('background--gradient');
-		$('.page-footer').toggleClass('page-footer--opened');
-		$('.page-main__header-title').removeClass('page-main__header-title--glass');
-
-		$('.workers-block').removeClass('workers-block--closed');
-		$('.workers-block').addClass('workers-block--opened');
+	if($('.glass-block').hasClass('glass-block--opened')){
+			$('.page-header').removeClass('page-header--glass');
+			$('.page-header').addClass('page-header--transparent');
+	
+			$('.glass-block').removeClass('glass-block--closed glass-block--opened');
+			$('.glass-block').addClass('glass-block--closed');
+	
+			$('body').addClass('background--gradient');
+			$('.page-footer__table').addClass('page-footer__table--closed');
+			$('.page-main__header-title').removeClass('page-main__header-title--glass');
+	
+			$('.workers-block').removeClass('workers-block--closed');
+			$('.workers-block').addClass('workers-block--opened');
+	
+			$('.page-footer__scroll-btn').addClass('page-footer__scroll-btn--black');
+		}
+		
 	} catch {
 		console.log("Классы уже переключены");
 	}
-});
+}
+
+$('.page-footer__scroll-btn').on('click', scroll);
+$(window).scroll(scroll);
